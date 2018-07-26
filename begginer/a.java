fcb7d009721965b1d83778ae196d8b496e85728f
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-07-26 05:52:46 -0400

    Create a.java

---
+++ begginer/a.java
@@ -0,0 +1,23 @@
+import.java.util.*;
+import java.util.Scanner;
+public class a;
+public static void main(String[]args)
+{
+int n;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter a number");
+n=s.nextInt();
+if(n<0)
+{
+System.out.prinln("n value is negative");
+}
+else if(n>0)
+{
+System.out.println("n value is positive");
+}
+else(n=0)
+{
+System.out.println("n value is zero");
+}
+
+
