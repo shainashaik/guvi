cc263d36e112138cdf4b46d78a0247b4af856ac0
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-07-26 13:34:17 -0400

    Update Alphabet.java

--- beginner/Alphabet.java
+++ beginner/Alphabet.java
@@ -3,20 +3,23 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java .util.Scanner;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		char c='a';
+		char c;
+		Scanner s=new Scanner(System.in);
+		c=s.next().charAt(0);
 		if((c>='a' && c<='z')||(c>='A' && c<='Z'))
 		{
-			System.out.println("Alaphabet");
+			System.out.println("Alphabet");
 		}
 		else
 		{
-			System.out.println("not Alaphabet");
+			System.out.println("not Alphabet");
 		}
 		}
 		
