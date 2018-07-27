b69ee4d4c74d26bc54c8098c8c122a5dfc02b881
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-07-27 06:01:21 -0400

    Create evennum.java

---
+++ evennum.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		for(int i=6; i<9;i++)
+		{
+			//System.out.println(i);
+			
+			if(i!=6){
+			
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+			}
+		
+		}
+			
+	}
+}
