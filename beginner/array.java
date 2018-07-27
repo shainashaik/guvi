0f1bb0fff94e610f6e2fafd93bbfb4231739d8c5
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-07-27 01:37:55 -0400

    Create array.java

---
+++ beginner/array.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n[]={1,3,3,4,5},k=2,i,sum=0;
+		for(i=0;i<k;i++)
+		{
+			sum=sum+n[i];
+		}
+		System.out.println(sum);
+	}
+}
