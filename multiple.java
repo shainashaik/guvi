f106088824ef741916393eb8d26d8019cde7794e
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-07-30 04:30:03 -0400

    Update multiple.java

--- multiple.java
+++ multiple.java
@@ -3,7 +3,6 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-import java.util.Scanner;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
@@ -15,7 +14,7 @@ class Ideone
 		n=s.nextInt();
 		for(i=1;i<=5;i++)
 		{
-			System.out.print((n*i+" "));
-	}
+			System.out.print(n*i+" ");
+		}
 	}
 }
