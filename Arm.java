15585570d51bc13341aa5df346f8da51d053f830
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-07-30 01:45:17 -0400

    Create Arm.java

---
+++ Arm.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int sum=0,n=371,a,temp;
+		temp=n;
+		while(n>0)
+		{
+			a=n%10;
+			n=n/10;
+			sum=sum+(a*a*a);
+		}
+		if(temp==sum)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+		}
+		}
+		
+	
