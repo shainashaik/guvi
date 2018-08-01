f5abfa4f3d16a3db3b32a3b2d77dc30d4e289868
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-01 05:32:34 -0400

    Create arth.java

---
+++ arth.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,a,d;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=s.nextInt();
+		d=s.nextInt();
+		System.out.println(n*(2*a+(n-1)*d)/2);
+	}
+}
