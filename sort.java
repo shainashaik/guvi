2036ab6342748d90cd8a9a8a225426cb3baf64b4
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-02 06:45:49 -0400

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,41 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,temp,i,j;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			for(j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+			}
+			for(i=0;i<n;i++)
+		
+			{
+				System.out.print(a[i]+" ");
+				}
+				System.out.print(a[n+1]);
+			}
+					
+				}
+		
