195b0e46c9817dbcaad7292627e492afad1e05e7
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-06 05:13:24 -0400

    Update character.java

--- character.java
+++ character.java
@@ -9,14 +9,16 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int count=0,i;
-		Scanner s=new Scanner(System.in);
-		String c=s.nextLine();
-		for(i=0;i<c.length();i++)
+	int count=0,i;
+	Scanner s=new Scanner(System.in);
+	String c=s.nextLine();
+	for(i=0;i<c.length();i++)
 		if(c.charAt(i)!=' ')
 		{
 			count++;
 		}
-		System.out.println(count);
+		System.out.print(count);
+		
+			
 		}
 	}
