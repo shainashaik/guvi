a576684cb2998a626d0a14df9dd6825d3508d0cc
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-14 07:07:41 -0400

    Update stringnumer.java

--- stringnumer.java
+++ stringnumer.java
@@ -5,23 +5,30 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class String
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	String n;
-	boolean b;
-	Scanner s=new Scanner(System.in);
-	n=s.next();
-	b=(n.matches("-?\\d+(\\.\\d+)?"));
-	if(b)
-	{
-		System.out.println("yes");
-	}
-	else
-	{
-		System.out.println("no");
-	}
-	}
-}
+		Scanner s=new Scanner(System.in);
+		String c=s.next();
+		boolean numeric=true;
+		try
+		{
+			Double num=Double.parseDouble(c);
+		}
+		catch (NumberFormatException e)
+		{
+			numeric=false;
+		}
+		if(numeric)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("No");
+		}
+		}
+		}
+	
 	
