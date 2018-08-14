789888b4bc92910c2ab7088fdbfc32a8fb4186c1
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-14 02:22:11 -0400

    Update stringcon.java

--- stringcon.java
+++ stringcon.java
@@ -5,13 +5,13 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class String
+class Stringcon
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner s=new Scanner(System.in);
-		String s1=s.next();
-		String s2=s.next();
-		System.out.print(s1+s2);
+	Scanner s=new Scanner(System.in);
+	String s1=s.next();
+	String s2=s.next();
+	System.out.print(s1+s2);
 	}
 }
