f31fed4cf5bc123c090631e41723eeb57aac6453
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-16 01:27:32 -0400

    Create alphdigit.java

---
+++ alphdigit.java
@@ -0,0 +1,42 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+int count=0;
+int count1=0;
+int count2=0;
+String c=s.nextLine();
+for(int i=0;i<c.length();i++)
+{
+	if(Character.isDigit(c.charAt(i)))
+	{
+		count++;
+	}
+		else if(Character.isAlphabetic(c.charAt(i)))
+		{
+			count1++;
+		}
+		else
+		{
+			count2++;
+		}
+}
+if(count>0&&count1>0)
+{
+	System.out.println("Yes");
+}
+else
+{
+	System.out.println("No");
+}
+}
+		}
+		
