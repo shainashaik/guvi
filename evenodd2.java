1ac53a749f37dc0f823df4e1c2f64e58a8fe9e1f
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-17 01:19:05 -0400

    Create evenodd2.java

---
+++ evenodd2.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,m,a;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		m=s.nextInt();
+		a=n+m;
+		if(a%2==0)
+		{
+			System.out.print("even");
+		}
+		else
+		{
+			System.out.print("odd");
+		}
+		}
+		
+	}
