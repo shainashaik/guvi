43189357630cc18c4583baab862ded9ae16c8b25
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-17 03:29:24 -0400

    Rename multiple.java to multiple10.java

---
+++ multiple10.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n,a,b;
+		n=s.nextInt();
+		a=(n/10)*10;
+		b=a+10;
+		{
+			System.out.print(b);
+		}
+	}
+	}
