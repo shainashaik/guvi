a11863b4e0893748102129db74be390ee9d5b365
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-23 23:25:16 -0400

    Create ptr.java

---
+++ ptr.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int p,t,r,si;
+		Scanner s=new Scanner(System.in);
+		p=s.nextInt();
+		t=s.nextInt();
+		r=s.nextInt();
+		si=(p*t*r)/100;
+		System.out.print(si);
+	}
+}
