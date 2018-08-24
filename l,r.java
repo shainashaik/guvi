99aeac4e2bce4baf975877466db966d49a65c607
shainashaik <41730936+shainashaik@users.noreply.github.com>
2018-08-23 22:59:39 -0400

    Create l,r.java

---
+++ l,r.java
@@ -0,0 +1,45 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int l,r,i,n;
+		Scanner s=new Scanner(System.in);
+		l=s.nextInt();
+		r=s.nextInt();
+		n=s.nextInt();
+		boolean flag=false;
+		for(i=l+1;i<r;i++);
+			{
+			if(n%i==0)
+			{
+				flag=true;
+			}
+			}
+			if(flag==true)
+			{
+{
+	
+}			
+				System.out.print("yes");
+			}
+			
+			
+			else
+			{
+			
+				System.out.print("no");
+			}
+			}
+			
+			
+			
+			}
+		
+	
